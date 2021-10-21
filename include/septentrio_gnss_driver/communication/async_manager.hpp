69a0de42b36fb82328994d98fb42049b510f692a
tibordome <tibor.doeme@alumni.ethz.ch>
2021-10-21 21:39:48 +0100

    Removed pointer-related bug in async_manager.hpp

--- include/septentrio_gnss_driver/communication/async_manager.hpp
+++ include/septentrio_gnss_driver/communication/async_manager.hpp
@@ -251,14 +251,14 @@ namespace io_comm_rx {
 
             try
             {
-                ROS_DEBUG(
+			    ROS_DEBUG(
                     "Calling read_callback_() method, with number of bytes to be parsed being %li",
                     arg_for_read_callback);
                 read_callback_(to_be_parsed_, arg_for_read_callback);
             } catch (std::size_t& parsing_failed_here)
             {
-    			to_be_parsed_ += parsing_failed_here;
-    			arg_for_read_callback -= parsing_failed_here;
+			    to_be_parsed_ += parsing_failed_here;
+			    arg_for_read_callback -= parsing_failed_here;
 				ROS_DEBUG(
                     "Current buffer size is %li and parsing_failed_here is %li",
                     current_buffer_size, parsing_failed_here);
