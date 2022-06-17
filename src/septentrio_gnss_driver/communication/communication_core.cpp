be50f12e8a6f9a84e45a58aa9d7889fb3dbfbd7a
Thomas Emter <thomas.emter@iosb.fraunhofer.de>
2022-06-18 00:16:38 +0200

    Fix memory corruption

--- src/septentrio_gnss_driver/communication/communication_core.cpp
+++ src/septentrio_gnss_driver/communication/communication_core.cpp
@@ -1131,7 +1131,7 @@ bool io_comm_rx::Comm_IO::initializeTCP(std::string host, std::string port)
 void io_comm_rx::Comm_IO::initializeSBFFileReading(std::string file_name)
 {
     node_->log(LogLevel::DEBUG, "Calling initializeSBFFileReading() method..");
-    std::size_t buffer_size = 131072;
+    std::size_t buffer_size = 8192;
     uint8_t* to_be_parsed;
     to_be_parsed = new uint8_t[buffer_size];
     std::ifstream bin_file(file_name, std::ios::binary);
