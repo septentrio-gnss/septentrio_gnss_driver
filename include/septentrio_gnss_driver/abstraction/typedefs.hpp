e824fa5ecdc1422a8fc99eaabd62ef4395dfa4b0
Thomas Emter <thomas.emter@iosb.fraunhofer.de>
2023-12-20 11:46:50 +0100

    Change pointer type

--- include/septentrio_gnss_driver/abstraction/typedefs.hpp
+++ include/septentrio_gnss_driver/abstraction/typedefs.hpp
@@ -430,14 +430,14 @@ public:
     bool hasImprovedVsmHandling() { return capabilities_.has_improved_vsm_handling; }
 
 private:
-    void callbackOdometry(const nav_msgs::msg::Odometry::SharedPtr odo)
+    void callbackOdometry(const nav_msgs::msg::Odometry::ConstSharedPtr& odo)
     {
         Timestamp stamp = timestampFromRos(odo->header.stamp);
 
         processTwist(stamp, odo->twist);
     }
 
-    void callbackTwist(const TwistWithCovarianceStampedMsg::SharedPtr twist)
+    void callbackTwist(const TwistWithCovarianceStampedMsg::ConstSharedPtr& twist)
     {
         Timestamp stamp = timestampFromRos(twist->header.stamp);
 
