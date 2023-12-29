a06b5829730e42aaab2bb43464d802883772affb
Thomas Emter <thomas.emter@iosb.fraunhofer.de>
2023-12-29 14:59:00 +0100

    Move IPS ids check

--- src/septentrio_gnss_driver/node/rosaic_node.cpp
+++ src/septentrio_gnss_driver/node/rosaic_node.cpp
@@ -539,6 +539,46 @@ rosaic_node::ROSaicNode::ROSaicNode(const rclcpp::NodeOptions& options) :
         }
     }
 
+    // Check uniqueness of IPS ids
+    if (!settings_.tcp_ip_server.empty())
+    {
+        if (settings_.tcp_ip_server == settings_.udp_ip_server)
+            this->log(
+                log_level::ERROR,
+                "tcp.ip_server and udp.ip_server cannot use the same IP server");
+        for (size_t i = 0; i < settings_.rtk.ip_server.size(); ++i)
+        {
+            if (settings_.tcp_ip_server == settings_.rtk.ip_server[i].id)
+                this->log(log_level::ERROR,
+                          "tcp.ip_server and rtk_settings.ip_server_" +
+                              std::to_string(i + 1) +
+                              ".id cannot use the same IP server");
+        }
+    }
+    if (!settings_.udp_ip_server.empty())
+    {
+        if (settings_.udp_ip_server == settings_.ins_vsm.ip_server)
+            this->log(
+                log_level::ERROR,
+                "udp.ip_server and ins_vsm.ip_server.id cannot use the same IP server");
+        for (size_t i = 0; i < settings_.rtk.ip_server.size(); ++i)
+        {
+            if (settings_.udp_ip_server == settings_.rtk.ip_server[i].id)
+                this->log(log_level::ERROR,
+                          "udp.ip_server and rtk_settings.ip_server_" +
+                              std::to_string(i + 1) +
+                              ".id cannot use the same IP server");
+        }
+    }
+    if (settings_.rtk.ip_server.size() == 2)
+    {
+        if (!settings_.rtk.ip_server[0].id.empty() &&
+            (settings_.rtk.ip_server[0].id == settings_.rtk.ip_server[1].id))
+            this->log(
+                log_level::ERROR,
+                "rtk_settings.ip_server_1.id and rtk_settings.ip_server_2.id cannot use the same IP server");
+    }
+
     // VSM - velocity sensor measurements for INS
     if (settings_.septentrio_receiver_type == "ins")
     {
@@ -658,36 +698,6 @@ rosaic_node::ROSaicNode::ROSaicNode(const rclcpp::NodeOptions& options) :
             registerSubscriber();
         }
 
-        if (!settings_.tcp_ip_server.empty())
-        {
-            if (settings_.tcp_ip_server == settings_.udp_ip_server)
-                this->log(
-                    log_level::ERROR,
-                    "tcp.ip_server and udp.ip_server cannot use the same IP server");
-            for (size_t i = 0; i < settings_.rtk.ip_server.size(); ++i)
-            {
-                if (settings_.tcp_ip_server == settings_.rtk.ip_server[i].id)
-                    this->log(log_level::ERROR,
-                              "tcp.ip_server and rtk_settings.ip_server_" +
-                                  std::to_string(i + 1) +
-                                  ".id cannot use the same IP server");
-            }
-        }
-        if (!settings_.udp_ip_server.empty())
-        {
-            if (settings_.udp_ip_server == settings_.ins_vsm.ip_server)
-                this->log(
-                    log_level::ERROR,
-                    "udp.ip_server and ins_vsm.ip_server.id cannot use the same IP server");
-            for (size_t i = 0; i < settings_.rtk.ip_server.size(); ++i)
-            {
-                if (settings_.udp_ip_server == settings_.rtk.ip_server[i].id)
-                    this->log(log_level::ERROR,
-                              "udp.ip_server and rtk_settings.ip_server_" +
-                                  std::to_string(i + 1) +
-                                  ".id cannot use the same IP server");
-            }
-        }
         if (!settings_.ins_vsm.ip_server.empty())
         {
             for (size_t i = 0; i < settings_.rtk.ip_server.size(); ++i)
@@ -698,14 +708,6 @@ rosaic_node::ROSaicNode::ROSaicNode(const rclcpp::NodeOptions& options) :
                                   std::to_string(i + 1) +
                                   ".id cannot use the same IP server");
             }
-        }
-        if (settings_.rtk.ip_server.size() == 2)
-        {
-            if (!settings_.rtk.ip_server[0].id.empty() &&
-                (settings_.rtk.ip_server[0].id == settings_.rtk.ip_server[1].id))
-                this->log(
-                    log_level::ERROR,
-                    "rtk_settings.ip_server_1.id and rtk_settings.ip_server_2.id cannot use the same IP server");
         }
     }
 
